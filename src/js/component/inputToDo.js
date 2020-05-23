c4c0e4b3f7a85e6b6856f7f667a7ac6228821c70
Juan Pollan <juanferformacionyproyectos@gmail.com>
2020-05-23 20:19:29 +0000

    configurado arreglo evento click y keypress

--- src/js/component/inputToDo.js
+++ src/js/component/inputToDo.js
@@ -1,6 +1,5 @@
 import React from "react";
-//import List from "./list.js";
-
+//import List from "./list.js";2
 export class InputToDo extends React.Component {
 	constructor() {
 		super();
@@ -9,70 +8,40 @@ export class InputToDo extends React.Component {
 			userInput: ""
 		};
 	}
-	changeUserInput(input) {
-		console.log(input);
-		this.setState({
-			userInput: input
-		});
-		console.log(this.state.userInput);
-	}
 
 	addToList(e) {
-		if (e.key === "Enter") {
-			console.log(e.state);
-			let listArray = this.state.list.concat(e);
-			console.log(listArray);
-			//listArray.push(input);
-			//console.log(listArray.push(input));
+		if (e.which == 13 || e.keyCode == 13) {
+			let listArray = this.state.list.concat(this.state.userInput);
 			this.setState({
 				list: listArray,
 				userInput: ""
 			});
 		}
 	}
-
-	btnToList(input) {
-		//event.preventDefault();
-		let listArray = this.state.list.concat(input);
-		console.log(listArray);
-		//listArray.push(input);
-		//console.log(listArray.push(input));
+	btnToList() {
+		let listArray = this.state.list.concat(this.state.userInput);
 		this.setState({
 			list: listArray,
 			userInput: ""
 		});
 	}
-
-	/*taskCounter = leng => {
-		if (leng === 0) {
-			return (
-				<li className="taskCounter text-muted">No tasks, add a task</li>
-			);
-		} else if (leng === 1) {
-			return <li className="taskCounter text-muted">{leng} item left</li>;
-		} else if (leng > 1) {
-			return (
-				<li className="taskCounter text-muted">{leng} items left</li>
-			);
-		}
-	};*/
-
 	render() {
 		return (
 			<div className="main">
 				<h1>todos</h1>
 				<input
 					type="text"
-					placeholder="What need to be done?"
-					//onKeyPress={this.addToList}
-					onChange={e => this.changeUserInput(e.target.value)}
+					placeholder="TASK TO DO?"
+					onChange={e => {
+						this.setState({
+							...this.state,
+							userInput: e.target.value
+						});
+					}}
 					value={this.state.userInput}
-					onKeyPress={() => this.addToList(this.state.userInput)}
+					onKeyPress={e => this.addToList(e)}
 				/>
-				<button onClick={() => this.btnToList(this.state.userInput)}>
-					Picnha
-				</button>
-
+				<button onClick={() => this.btnToList()}>Pincha</button>
 				<ul>
 					{this.state.list.map((element, i) => (
 						<li key={i}>{element}</li>
